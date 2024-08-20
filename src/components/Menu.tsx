ac846140f9413e72cb5d38659d4559550708ff0d
Princy Patel <kapatelprincy@gmail.com>
2024-08-20 22:05:27 +0530

    remove the padding

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ import { IData } from "../@types/data";
 
 function Menu() {
   return (
-    <div className="flex flex-col px-4 py-6 sm:px-6 lg:px-8">
+    <div className="flex flex-col px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-bold mb-4 sm:text-4xl">Desserts</h1>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map((dataItem: IData, index: number) => (
