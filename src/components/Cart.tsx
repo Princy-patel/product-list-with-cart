9ce0aedb1ac9d885be2043f4eeab090d8593ed0b
Princy Patel <kapatelprincy@gmail.com>
2024-08-25 11:41:36 +0530

    fix: calculate total price of items in the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../common/Button";
 import { RootState } from "../store/store";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { updateQuantity, removeItemsFromCart } from "../slices/productSlice";
 import CartModal from "./CartModal";
 
@@ -16,7 +16,11 @@ function Cart() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const totalPrice = cartSelector.reduce((acc, curr) => acc + curr.price, 0);
+    const totalPrice = cartSelector.reduce(
+      (acc, item) => acc + item.quantity * item.price,
+      0
+    );
+
     setTotalCartPrice(totalPrice);
   }, [cartSelector]);
 
@@ -56,15 +60,21 @@ function Cart() {
         <>
           {cartSelector.map((item) => {
             return (
-              <>
-                <div className="flex">
-                  <p className="m-2">{item.name}</p>
-                  <div className="flex justify-center items-center">
-                    <Button onClick={decrementItems.bind(null, item.id)}>
+              <Fragment key={item.id}>
+                <div className="flex justify-between items-center w-full">
+                  <p className="m-2 my-4">{item.name}</p>
+                  <div className="my-2 border-2 border-[#8f4533] flex leading-3 justify-center items-center space-x-4 px-3 py-1 rounded-full">
+                    <Button
+                      className="text-xl"
+                      onClick={decrementItems.bind(null, item.id)}
+                    >
                       -
                     </Button>
-                    <p>{item.quantity}</p>
-                    <Button onClick={incrementItems.bind(null, item.id)}>
+                    <p className="text-xl font-semibold">{item.quantity}</p>
+                    <Button
+                      className="text-xl"
+                      onClick={incrementItems.bind(null, item.id)}
+                    >
                       +
                     </Button>
                   </div>
@@ -72,22 +82,24 @@ function Cart() {
 
                 <div className="flex justify-between items-center">
                   <div className="[&>*]:px-2 mb-2">
-                    <span className="text-[#8f4533] font-bold">1x</span>
-                    <span>@ ${item.price}</span>
+                    <span className="text-[#8f4533] font-bold">
+                      {item.quantity} x
+                    </span>
                     <span>${item.price}</span>
+                    <span>${item.price * item.quantity}</span>
                   </div>
 
                   <div>
                     <img
                       src="/images/icon-remove-item.svg"
                       alt="remove-icon"
-                      className="border border-[#8f4533] rounded-full p-1 cursor-pointer text-black hover:border-black"
+                      className="border border-[#010101] rounded-full p-1 cursor-pointer text-black hover:border-black"
                       onClick={removeItem.bind(null, item.id)}
                     />
                   </div>
                 </div>
                 <hr />
-              </>
+              </Fragment>
             );
           })}
 
